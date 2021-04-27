4a817ecccc715df56979723214b6626d2c1a6d6d
Barbara Barabas <bbarbara092@gmail.com>
2021-04-27 20:22:23 +0200

    save job details in the database

--- app/Http/Controllers/ApplicationController.php
+++ app/Http/Controllers/ApplicationController.php
@@ -2,9 +2,26 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Application;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
 
 class ApplicationController extends Controller
 {
-    //
+    function applyForJob(Request $request)
+    {
+        try{
+            $userId = auth()->user()->id;
+            Application::insert([  "user_id" => $userId,  "job_id" => $request->get('jobId'), "type" => $request->get("type"), "company" => $request->get('company'), "description" => $request->get('description')]);
+            return response()->json([
+                'status_code' => 200,
+                'message' => 'Save application'
+            ]);
+        }
+        catch (\Exception $e) {
+            Log::error($e->getMessage());
+            return response()->json(['message' => 'something went wrong'], 400);
+        }
+
+    }
 }
