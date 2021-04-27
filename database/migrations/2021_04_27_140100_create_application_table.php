f6f2a5dd81190f45604129c5d6c88821cd71d735
Barbara Barabas <bbarbara092@gmail.com>
2021-04-27 16:13:13 +0200

    create application table and migrate to the database

---
+++ database/migrations/2021_04_27_140100_create_application_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateApplicationTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('application', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('user_id');
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->string('job_id');
+            $table->string('type');
+            $table->string('company');
+            $table->string('title');
+            $table->string('description');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('application');
+    }
+}
